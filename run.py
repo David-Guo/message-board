d19123b2ec73a1b07d8bb7087f839b7ca50202e9
David-Guo <guodw3@mail2.sysu.edu.cn>
2015-11-30 23:39:09 +0800

    4.3 把表单渲染成HTML

--- run.py
+++ run.py
@@ -8,22 +8,46 @@ Created on Wed Nov 11 20:11:27 2015
 from flask import Flask, render_template
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
+from flask.ext.moment import Moment
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+
 manager = Manager(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
-@app.route('/')
+class NameForm(Form):
+    name = StringField('What is you name?', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.date
+        form.name.date = ''
+    return render_template('index.html', form=form, name=name)
+
 
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
 
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
 
 if __name__ == '__main__':
     manager.run()
